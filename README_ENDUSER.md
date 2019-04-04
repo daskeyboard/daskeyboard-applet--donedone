11d4cd4087bdb1079922e76e1151b1fe90624be6
F-DALLA-VALLE <dallavalle.fabien@gmail.com>
2019-04-04 17:21:32 -0500

    First commit. Applet's working

---
+++ README_ENDUSER.md
@@ -0,0 +1,11 @@
+# DoneDone
+
+Displays new tickets activity on a Das Keyboard Q Series.
+
+DoneDone is the #1 help desk & ticket tracking software to provide great customer support.
+
+For more information visit <https://getdonedone.com/>.
+
+![DoneDone on a Das Keyboard Q](assets/image.png "DoneDone applet")
+
+This applet uses the API from [DoneDone](https://www.getdonedone.com/api/).
---
+++ index.js
@@ -0,0 +1,155 @@
+// Library to send signal to Q keyboards
+const q = require('daskeyboard-applet');
+
+// Library to send request to API
+const request = require('request-promise');
+
+var dateFormat = require('dateformat');
+
+const btoa = require('btoa');
+
+const logger = q.logger;
+
+const baseUrl1 = 'https://';
+const baseUrl2 = '.mydonedone.com/issuetracker/api/v2'
+
+// Get the current time
+function getUtcTime() {
+  var now = new Date();
+  var utcTime = dateFormat(now, "isoUtcDateTime");
+  return utcTime;
+}
+
+// Test if an object is empty
+function isEmpty(obj) {
+  for(var key in obj) {
+      if(obj.hasOwnProperty(key))
+          return false;
+  }
+  return true;
+}
+
+
+class DoneDone extends q.DesktopApp {
+
+  constructor() {
+    super();
+    // run every 10 sec
+    this.pollingInterval = 10 * 1000;
+  }
+
+  async applyConfig() {
+
+    logger.info("DoneDone initialisation.")
+
+    this.subdomain = this.config.subdomain;
+    this.username = this.config.username;
+    
+    if(this.subdomain){
+
+      // Create time variable
+      this.now = new Date().getTime()/1000;
+      this.now = `${this.now}`.replace('.','');
+
+      this.baseUrl = baseUrl1 + this.subdomain + baseUrl2;
+      this.params = `${this.config.username}:${this.authorization.apiKey}`;
+      this.paramsBase64Encoded = btoa(this.params);
+    
+      this.serviceHeaders = {
+        "Authorization": `Basic ${this.paramsBase64Encoded}`,
+      }
+
+      logger.info("This is subdomain: "+this.subdomain);
+      logger.info("This is the base Url: "+this.baseUrl);
+
+      // Get the user ID
+      await request.get({
+        url: `${this.baseUrl}/people/me.json`,
+        headers: this.serviceHeaders,
+        json: true
+      }).then((body) => {
+        logger.info("Let's configure the user ID.");
+        logger.info("This is the body: "+ JSON.stringify(body));
+        this.userId = body.id;
+        logger.info("This is the userID: "+ this.userId);
+      })
+      .catch(error => {
+        logger.error(
+          `Got error sending request to service: ${JSON.stringify(error)}`);
+      });
+    }else{
+      logger.info("Subdomain is undefined. Configuration is not done yet");
+    }
+
+
+  }
+
+  // call this function every pollingInterval
+  async run() {
+    let signal;
+
+    try {
+      const body = await request.get({
+        url: `${this.baseUrl}/issues/all.json`,
+        headers: this.serviceHeaders,
+        json: true
+      });
+
+      logger.info("Looking for DoneDone issues");
+      logger.info("This is the date " + this.now);
+      logger.info("Issues response: " + JSON.stringify(body));
+
+      // Test if there is something inside the response
+      var isBodyEmpty = isEmpty(body) || (body === "[]");
+      if (isBodyEmpty) {
+        logger.info("Response empty when getting all issues.");
+        signal = null;
+      }
+      else {
+
+        for (let issue of body.issues) {
+          // extract the issues from the response
+          logger.info("This is how a issue looks: " + JSON.stringify(issue));
+          logger.info("CHECKING  "+issue.last_updated_on.slice(6,18));
+          logger.info("NOWWW  "+this.now);
+
+
+          if(issue.last_updated_on.slice(6,18) > this.now){
+            logger.info("Issue UDPATED");
+            signal = new q.Signal({
+              points: [[new q.Point(this.config.color, this.config.effect)]],
+              name: "DoneDone",
+              message: "TODO",
+              link: {
+                url: "https://www.google.com",
+                label: 'Show in DoneDone',
+              }
+            });
+            // Updated time
+            this.now = new Date().getTime()/1000;
+            this.now = `${this.now}`.replace('.','');
+
+          }
+        }
+
+
+        return signal;
+      }
+    }
+    catch (error) {
+      logger.error(`Got error sending request to service: ${JSON.stringify(error)}`);
+      return q.Signal.error([
+        'The DoneDone service returned an error. Please check your API key and account.',
+        `Detail: ${error.message}`
+      ]);
+    }
+
+  }
+
+}
+
+module.exports = {
+  DoneDone: DoneDone,
+};
+
+const doneDone = new DoneDone();
