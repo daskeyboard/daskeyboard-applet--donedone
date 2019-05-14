4a787baa4e374a50a29db2df6245f9ecc689cf0e
F-DALLA-VALLE <dallavalle.fabien@gmail.com>
2019-05-14 08:19:12 -0500

    Added comment.

--- index.js
+++ index.js
@@ -231,9 +231,10 @@ class DoneDone extends q.DesktopApp {
     catch (error) {
       logger.error(`Got error sending request to service: ${JSON.stringify(error)}`);
       if(`${error.message}`.includes("getaddrinfo")){
-        return q.Signal.error(
-          'The DoneDone service returned an error. <b>Please check your internet connection</b>.'
-        );
+        // Do not send error
+        // return q.Signal.error(
+        //   'The DoneDone service returned an error. <b>Please check your internet connection</b>.'
+        // );
       }else{
         return q.Signal.error([
           'The DoneDone service returned an error. <b>Please check your API key and account</b>.',
